a6d2c80afbadcc34c2eb27acc2a2fe686ec7347d
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-03-12 14:05:16 +0530

    view is coded

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
-class taskControler {
+class taskController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-    // document.getElementById("task").addEventListener("click", (event) => {
-    //   const task = document.getElementById("task").innerHTML;
-    //   // console.log(event);
-    //   // const newTask = ;
-    //   // model.storeTask();
-    // });
+    document.getElementById("add-btn").addEventListener("click", () => {
+      const task = document.getElementById("task").value;
+      this.model.addNewTask(task);
+      this.view.displayNewList(this.model.updatedTaskList());
+    });
   }
 }
 
-const controler = new taskControler(null, null);
+class Model {
+  #taskList;
+  constructor() {
+    this.#taskList = [];
+  }
+
+  addNewTask(task) {
+    this.#taskList.push(task);
+  }
+
+  updatedTaskList() {
+    return this.#taskList;
+  }
+}
+
+class View {
+  displayNewList(tasks) {
+    const display = document.getElementById("display");
+    tasks.map((task) => {
+      const taskElement = document.createElement("div");
+      taskElement.innerHTML = `<p>${task}</p>`;
+      display.appendChild(taskElement);
+    });
+  }
+}
+
+// const controler = new taskController(null, null);
+
+const model = new Model();
+model.addNewTask("playing basketball");
+console.log(model.updatedTaskList());
