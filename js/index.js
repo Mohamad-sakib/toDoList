dbee07aeb0a494b0b52f35b074e4b9c4e66d8c88
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-03-12 14:37:20 +0530

    controller,model and view created and function of adding new task is working

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ class taskController {
     this.model = model;
     this.view = view;
     document.getElementById("add-btn").addEventListener("click", () => {
-      const task = document.getElementById("task").value;
-      this.model.addNewTask(task);
+      const task = document.getElementById("task");
+      this.model.addNewTask(task.value);
       this.view.displayNewList(this.model.updatedTaskList());
+      task.value = "";
     });
   }
 }
@@ -28,16 +29,13 @@ class Model {
 class View {
   displayNewList(tasks) {
     const display = document.getElementById("display");
-    tasks.map((task) => {
-      const taskElement = document.createElement("div");
-      taskElement.innerHTML = `<p>${task}</p>`;
-      display.appendChild(taskElement);
-    });
+    const taskElement = document.createElement("div");
+    const newTask = tasks.at(-1);
+    taskElement.innerHTML = `<p>${newTask}</p>`;
+    display.appendChild(taskElement);
   }
 }
 
-// const controler = new taskController(null, null);
-
 const model = new Model();
-model.addNewTask("playing basketball");
-console.log(model.updatedTaskList());
+const view = new View();
+const controler = new taskController(model, view);
---
+++ toDO.md
@@ -0,0 +1,21 @@
+// const controler = new taskController(null, null);
+
+// const model = new Model();
+// model.addNewTask("playing basketball");
+// console.log(model.updatedTaskList());
+
+// const view = new View();
+// view.displayNewList([
+// "basketball",
+// "football",
+// "javascript",
+// "async and sync",
+// "basketball",
+// "football",
+// "javascript",
+// "async and sync",
+// "basketball",
+// "football",
+// "javascript",
+// "async and sync",
+// ]);
