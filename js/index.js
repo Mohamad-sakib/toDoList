3c6c7171cc39c044c65cf69385e6ceaeb6741d5f
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-03-12 12:36:16 +0530

    writing controller

---
+++ js/index.js
@@ -0,0 +1,14 @@
+class taskControler {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+    // document.getElementById("task").addEventListener("click", (event) => {
+    //   const task = document.getElementById("task").innerHTML;
+    //   // console.log(event);
+    //   // const newTask = ;
+    //   // model.storeTask();
+    // });
+  }
+}
+
+const controler = new taskControler(null, null);
