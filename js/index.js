c0e0218acc243726c86c26f1ed8aec9c1033fef6
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-03-13 09:42:00 +0530

    data model for do done

--- js/index.js
+++ js/index.js
@@ -1,41 +1,54 @@
 class taskController {
-  constructor(model, view) {
+  constructor(model) {
     this.model = model;
-    this.view = view;
+    // this.view = view;
     document.getElementById("add-btn").addEventListener("click", () => {
       const task = document.getElementById("task");
+      if (task.value === "") return;
       this.model.addNewTask(task.value);
-      this.view.displayNewList(this.model.updatedTaskList());
+      displayNewList(this.model.updatedTaskList());
       task.value = "";
     });
   }
 }
 
-class Model {
+class ToDoList {
   #taskList;
+  #newTaskId;
+
   constructor() {
-    this.#taskList = [];
+    this.#taskList = {};
+    this.#newTaskId = 1;
   }
 
   addNewTask(task) {
-    this.#taskList.push(task);
+    this.#taskList[this.#newTaskId] = task;
   }
 
   updatedTaskList() {
     return this.#taskList;
   }
-}
 
-class View {
-  displayNewList(tasks) {
-    const display = document.getElementById("display");
-    const taskElement = document.createElement("div");
-    const newTask = tasks.at(-1);
-    taskElement.innerHTML = `<p>${newTask}</p>`;
-    display.appendChild(taskElement);
+  deleteTask(taskId) {
+    delete this.#taskList[taskId];
   }
 }
 
-const model = new Model();
-const view = new View();
-const controler = new taskController(model, view);
+//view
+const displayNewList = (tasks) => {
+  const display = document.getElementById("display");
+  const taskElement = document.createElement("div");
+  const newTask = tasks.at(-1);
+  taskElement.innerHTML = `<span>${newTask}</span> <input type="checkbox"> `;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = `delete`;
+  display.appendChild(taskElement);
+  taskElement.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click", () => {
+    taskElement.remove();
+  });
+};
+
+const toDoList = new ToDoList();
+// const view = new View();
+// const controller = new taskController(toDoList);
