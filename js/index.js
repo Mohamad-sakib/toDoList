c0471496fff6caa31aea548b0862f4de85bf0980
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2025-03-17 00:40:41 +0530

    toDoList Logic for current version done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-class taskController {
-  constructor(model) {
-    this.model = model;
-    // this.view = view;
-    document.getElementById("add-btn").addEventListener("click", () => {
-      const task = document.getElementById("task");
-      if (task.value === "") return;
-      this.model.addNewTask(task.value);
-      displayNewList(this.model.updatedTaskList());
-      task.value = "";
-    });
-  }
-}
-
 class ToDoList {
   #taskList;
   #newTaskId;
@@ -22,7 +8,11 @@ class ToDoList {
   }
 
   addNewTask(task) {
+    console.log(task);
     this.#taskList[this.#newTaskId] = task;
+    this.#newTaskId += 1;
+    this.display();
+    return this.#newTaskId - 1;
   }
 
   updatedTaskList() {
@@ -31,24 +21,48 @@ class ToDoList {
 
   deleteTask(taskId) {
     delete this.#taskList[taskId];
+    this.display();
+  }
+
+  display() {
+    console.log(this.#taskList);
   }
 }
 
-//view
-const displayNewList = (tasks) => {
+const deleteTask = (event) => {
+  const target = event.target;
+  const taskElement = event.currentTarget;
+  const id = taskElement.id;
+  if ([...target.classList].includes("delete-btn")) {
+    taskElement.remove();
+    toDoList.deleteTask(id);
+  }
+};
+
+// //view
+const displayNewList = (tasks, taskId) => {
   const display = document.getElementById("display");
   const taskElement = document.createElement("div");
-  const newTask = tasks.at(-1);
+  const newTask = tasks[taskId];
   taskElement.innerHTML = `<span>${newTask}</span> <input type="checkbox"> `;
+  taskElement.id = taskId;
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = `delete`;
+  deleteBtn.classList.add("delete-btn");
   display.appendChild(taskElement);
   taskElement.appendChild(deleteBtn);
-  deleteBtn.addEventListener("click", () => {
-    taskElement.remove();
-  });
+  taskElement.addEventListener("click", deleteTask);
 };
 
 const toDoList = new ToDoList();
-// const view = new View();
-// const controller = new taskController(toDoList);
+
+window.addEventListener("load", function () {
+  document.getElementById("add-btn").addEventListener("click", () => {
+    const taskField = document.getElementById("task-field");
+
+    if (taskField.value === "") return;
+    const newTaskId = toDoList.addNewTask(taskField.value);
+    displayNewList(toDoList.updatedTaskList(), newTaskId);
+    taskField.value = "";
+  });
+});
